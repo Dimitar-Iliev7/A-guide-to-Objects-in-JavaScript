693cfb3f85dab13126e46acd08b1fb40dcc0aeba
Dimitar-Iliev7 <dimitar7iliev@yahoo.com>
2023-05-13 21:28:07 +0300

    Objects Guide

---
+++ main.js
@@ -0,0 +1,15 @@
+// Objects in JavaScript refer to a group of properties.
+//  Properties consist of key-value pairs, where the key is represented by a string,
+// and the value can be of any data type, including another object.
+//  In JavaScript almost anything can be treated as an object, and each object can contain properties with values.
+//  In simple terms, objects are like Rockstar's of JavaScript
+//  - they come in style, save the day, and organize your data like a boss.
+
+// object literals syntax
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 35,
+  country: "USA",
+  bike: "Harley Davidson",
+};
